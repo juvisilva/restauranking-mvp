80fd0a0417b9d47e361a1fbc825f28bc4094241c
juvisilva <jsilva@sanbar.pe>
2025-06-18 23:37:58 -0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Restauranking.pe</title>
+      </Head>
+      <main>
+        <h1>Restauranking.pe</h1>
+        <h2>Inteligencia Gastronómica Colectiva</h2>
+        <p>Esta es una base mínima de la aplicación.</p>
+      </main>
+    </>
+  )
+}
