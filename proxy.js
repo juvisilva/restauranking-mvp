eaf0b65d985523cb6a66fe359b8e2c670ff13b65
juvisilva <jsilva@sanbar.pe>
2025-07-10 20:40:43 -0500

    Delete proxy.js

--- proxy.js
+++
@@ -1,9 +0,0 @@
-
-export default async function handler(req, res) {
-  const { url } = req.query;
-  if (!url) return res.status(400).json({ error: "Missing URL" });
-
-  const response = await fetch(url);
-  const data = await response.json();
-  res.status(200).json(data);
-}
