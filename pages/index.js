243256bf3eb10a81b7ae4b630668c004092321e9
juvisilva <jsilva@sanbar.pe>
2025-07-11 11:46:05 -0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Bienvenido a Restauranking.pe</h1>
+      <p>Este es el inicio del proyecto con Next.js funcionando.</p>
+    </main>
+  );
+}
