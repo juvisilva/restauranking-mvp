04ead610a93b63ce811d975d820d1a5d9dd8ee35
juvisilva <jsilva@sanbar.pe>
2025-07-11 11:58:49 -0500

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Bienvenido a Restauranking.pe</h1>
-      <p>Este es el inicio del proyecto con Next.js funcionando.</p>
-    </main>
-  );
-}
